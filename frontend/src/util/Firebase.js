5ff2e880789126428c310bce56d12ba20b2ae138
schradt <schradt@wwu.edu>
2022-03-17 17:12:17 -0700

    Pushing code for react ecommerce site

---
+++ frontend/src/util/Firebase.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "XXX",
+  authDomain: "XXX",
+  projectId: "XXX",
+  storageBucket: "XXX",
+  messagingSenderId: "XXX",
+  appId: "XXX"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
